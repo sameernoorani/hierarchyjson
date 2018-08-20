12f7b104ba75f5258b08df847c4fb97e9b921192
slkmane <samappl3s@gmail.com>
2018-08-19 21:41:48 -0400

    Remove un-needed functions

--- app/Http/Controllers/Employees.php
+++ app/Http/Controllers/Employees.php
@@ -96,7 +96,6 @@ class Employees extends Controller
         $not_managers = self::notManagers($employees_data, $employees, $employee_array);
 
 
-
         $boss = array_diff_key($employee_array, $employees_data);
 
         /* add bosses to the $employees_data */
@@ -117,10 +116,9 @@ class Employees extends Controller
             return "There is a loop.";
         }
 
-        if ($count != count($employees)){
-          /* looks like there was a repeated/duplicate key data */
-          
-          return "There is a loop.";
+        if ($count != count($employees)) {
+            /* looks like there was a repeated/duplicate key data */
+            return "There is a loop.";
         }
 
 
@@ -195,69 +193,4 @@ class Employees extends Controller
         return self::buildTree($data, $count);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Post $post)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Post $post)
-    {
-        //
-    }
 }
